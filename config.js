52f04905279256db9c7d725f927bf73e592acc7f
isurahupa-tech <isurahupa@gmail.com>
2026-05-13 15:01:00 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "WFcnmZBJ#hFXFOrQHnSJLleeiIMf2KyGzWXYwOsUdts4hTWzqCY0",
+SESSION_ID: process.env.SESSION_ID || "3csSnTQD#OtP5E85nzpuIZVCPhw81TVA3pCWc6Rydc_8yz3JIFSo",
 ALIVE_IMG : process.env.ALIVE_IMG || "https://upld.zone.id/uploads/d4i0x5iq/sandes-md-v2.webp",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", 
 };
